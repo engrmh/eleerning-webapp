cdc0d97285081cb4ddfb64829b42d86f4530bbee
Mohammad Hosein <en.mohammadhosein@gmail.com>
2023-07-23 22:38:35 +0330

    feat: routing project

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import Category from "./Pages/Category/Category";
+import Index from "./Pages/Index/Index";
+import CourseInfo from "./Pages/CourseInfo/CourseInfo";
+import ArticleInfo from "./Pages/ArticleInfo/ArticleInfo";
+
+const routes = [
+  {
+    path: "/",
+    element: <Index />,
+  },
+  {
+    path: "/course-info/:courseName",
+    element: <CourseInfo />,
+  },
+  {
+    path: "/category-info/:categoryName",
+    element: <Category />,
+  },
+  {
+    path: "/article-info/:articleName",
+    element: <ArticleInfo />,
+  },
+];
+export default routes;
