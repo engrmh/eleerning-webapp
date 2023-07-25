f5e981a86267c5e1f2bac916d1abc2c7ed450220
Mohammad Hosein <en.mohammadhosein@gmail.com>
2023-07-25 23:11:47 +0330

    feat: about us section in home page

--- src/Pages/Index/Index.jsx
+++ src/Pages/Index/Index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
 import LastCourses from "../../Components/LastCuorses/LastCourses";
+import AboutUs from "../../Components/AboutUs/AboutUs";
 
 export default function Index() {
   return (
-    <div>
+    <>
       <Header />
       <LastCourses />
-    </div>
+      <AboutUs/>
+    </>
   );
 }
