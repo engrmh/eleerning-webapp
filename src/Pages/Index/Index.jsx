bbcb3dc8a8c0e3069007c4e990a7c16041ef9230
Mohammad Hosein <en.mohammadhosein@gmail.com>
2023-07-25 09:48:35 +0330

    feat: landing $ typewriter

--- src/Pages/Index/Index.jsx
+++ src/Pages/Index/Index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from "../../Components/Header/Header";
+import Landing from "../../Landing/Landing";
 
 export default function Index () {
   return (
     <div>
         <Header/>
+        <Landing/>
     </div>
   )
 }
