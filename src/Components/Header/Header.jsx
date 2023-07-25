b2c483d24e7c198c6746626535eae4cb882740a2
Mohammad Hosein <en.mohammadhosein@gmail.com>
2023-07-25 23:15:52 +0330

    refactor: change landing component directory address

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Topbar from "../Topbar/Topbar";
 import Navbar from "../Navbar/Navbar";
-import Landing from "../../Landing/Landing";
+import Landing from "../Landing/Landing";
 
 export default function Header () {
   return (
