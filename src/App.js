1ee225c78617eb97c9b72ce5525e482927c9ac13
Mohammad Hosein <en.mohammadhosein@gmail.com>
2023-07-30 13:50:12 +0330

    feat: category page & pagination | refactor: app.js for correct routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
-import {useRoutes} from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 import routes from "./routes";
+import Topbar from "./Components/Topbar/Topbar";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   const router = useRoutes(routes);
-  return <>{router}</>;
+  return (
+    <>
+      <div className="">
+        <div className="">
+          <Topbar />
+          <Navbar />
+        </div>
+        <div className="">{router}</div>
+        <div className="">
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
