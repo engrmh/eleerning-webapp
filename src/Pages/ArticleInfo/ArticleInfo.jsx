4df1252ad7e3ef80e99d6af6b5dc5c5c9c52746c
Mohammad Hosein <en.mohammadhosein@gmail.com>
2023-07-30 23:18:32 +0330

    feat: article side bar

--- src/Pages/ArticleInfo/ArticleInfo.jsx
+++ src/Pages/ArticleInfo/ArticleInfo.jsx
@@ -213,7 +213,8 @@ export default function ArticleInfo() {
                         <i class="fas fa-arrow-right suggestion-articles__icon"></i>
                       </a>
                       <a href="#" class="suggestion-articles__link">
-                        سریع ترین و بهترین راه یادگیری جاوا اسکریپت چیست؟ | تجربه برنامه نویسان
+                        سریع ترین و بهترین راه یادگیری جاوا اسکریپت چیست؟ |
+                        تجربه برنامه نویسان
                       </a>
                     </div>
                   </div>
@@ -223,17 +224,222 @@ export default function ArticleInfo() {
                         <i class="fas fa-arrow-left suggestion-articles__icon"></i>
                       </a>
                       <a href="#" class="suggestion-articles__link">
-                        سریع ترین و بهترین راه یادگیری جاوا اسکریپت چیست؟ | تجربه برنامه نویسان
+                        سریع ترین و بهترین راه یادگیری جاوا اسکریپت چیست؟ |
+                        تجربه برنامه نویسان
                       </a>
                     </div>
                   </div>
                 </div>
               </div>
               {/*  */}
-              <CommentsTextArea/>
+              <CommentsTextArea />
             </div>
             {/*  */}
-            <div class="col-4"></div>
+            <div class="col-4">
+              <div class="courses-info">
+                <div class="course-info">
+                  <span class="course-info__courses-title">
+                    پر امتیازترین دوره ها
+                  </span>
+                  <ul class="course-info__courses-list">
+                    <li class="course-info__courses-list-item">
+                      <a href="#" class="course-info__courses-link">
+                        <img
+                          src="/images/courses/js_project.png"
+                          alt="Course Cover"
+                          class="course-info__courses-img"
+                        />
+                        <span class="course-info__courses-text">
+                          پروژه های تخصصی با جاوا اسکریپت
+                        </span>
+                      </a>
+                    </li>
+                    <li class="course-info__courses-list-item">
+                      <a href="#" class="course-info__courses-link">
+                        <img
+                          src="/images/courses/fareelancer.png"
+                          alt="Course Cover"
+                          class="course-info__courses-img"
+                        />
+                        <span class="course-info__courses-text">
+                          تعیین قیمت پروژه های فریلنسری
+                        </span>
+                      </a>
+                    </li>
+                    <li class="course-info__courses-list-item">
+                      <a href="#" class="course-info__courses-link">
+                        <img
+                          src="/images/courses/nodejs.png"
+                          alt="Course Cover"
+                          class="course-info__courses-img"
+                        />
+                        <span class="course-info__courses-text">
+                          دوره Api نویسی
+                        </span>
+                      </a>
+                    </li>
+                    <li class="course-info__courses-list-item">
+                      <a href="#" class="course-info__courses-link">
+                        <img
+                          src="/images/courses/jango.png"
+                          alt="Course Cover"
+                          class="course-info__courses-img"
+                        />
+                        <span class="course-info__courses-text">
+                          متخصص جنگو
+                        </span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div class="course-info">
+                  <span class="course-info__courses-title">دسترسی سریع</span>
+                  <ul class="sidebar-articles__list">
+                    <li class="sidebar-articles__item">
+                      <i class="fas fa-angle-left sidebar-articles__icon"></i>
+                      <a href="#" class="sidebar-articles__link">
+                        صفحه اصلی
+                      </a>
+                    </li>
+                    <li class="sidebar-articles__item">
+                      <i class="fas fa-angle-left sidebar-articles__icon"></i>
+                      <a href="#" class="sidebar-articles__link">
+                        فرانت اند
+                      </a>
+                    </li>
+                    <li class="sidebar-articles__item">
+                      <i class="fas fa-angle-left sidebar-articles__icon"></i>
+                      <a href="#" class="sidebar-articles__link">
+                        امنیت
+                      </a>
+                    </li>
+                    <li class="sidebar-articles__item">
+                      <i class="fas fa-angle-left sidebar-articles__icon"></i>
+                      <a href="#" class="sidebar-articles__link">
+                        پایتون
+                      </a>
+                    </li>
+                    <li class="sidebar-articles__item">
+                      <i class="fas fa-angle-left sidebar-articles__icon"></i>
+                      <a href="#" class="sidebar-articles__link">
+                        مهارت های نرم
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div class="course-info">
+                  <span class="course-info__courses-title">مقاله های جدید</span>
+                  <ul class="last-articles__list">
+                    <li class="last-articles__item">
+                      <a href="#" class="last-articles__link">
+                        نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون
+                      </a>
+                    </li>
+                    <li class="last-articles__item">
+                      <a href="#" class="last-articles__link">
+                        نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون
+                      </a>
+                    </li>
+                    <li class="last-articles__item">
+                      <a href="#" class="last-articles__link">
+                        نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون
+                      </a>
+                    </li>
+                    <li class="last-articles__item">
+                      <a href="#" class="last-articles__link">
+                        نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون
+                      </a>
+                    </li>
+                    <li class="last-articles__item">
+                      <a href="#" class="last-articles__link">
+                        نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون
+                      </a>
+                    </li>
+                    <li class="last-articles__item">
+                      <a href="#" class="last-articles__link">
+                        نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div class="course-info">
+                  <span class="course-info__courses-title">
+                    دسته بندی مقالات
+                  </span>
+                  <ul class="sidebar-articles__list">
+                    <li class="sidebar-articles__item">
+                      <i class="fas fa-angle-left sidebar-articles__icon"></i>
+                      <a href="#" class="sidebar-articles__link">
+                        صفحه اصلی
+                      </a>
+                    </li>
+                    <li class="sidebar-articles__item">
+                      <i class="fas fa-angle-left sidebar-articles__icon"></i>
+                      <a href="#" class="sidebar-articles__link">
+                        فرانت اند
+                      </a>
+                    </li>
+                    <li class="sidebar-articles__item">
+                      <i class="fas fa-angle-left sidebar-articles__icon"></i>
+                      <a href="#" class="sidebar-articles__link">
+                        امنیت
+                      </a>
+                    </li>
+                    <li class="sidebar-articles__item">
+                      <i class="fas fa-angle-left sidebar-articles__icon"></i>
+                      <a href="#" class="sidebar-articles__link">
+                        پایتون
+                      </a>
+                    </li>
+                    <li class="sidebar-articles__item">
+                      <i class="fas fa-angle-left sidebar-articles__icon"></i>
+                      <a href="#" class="sidebar-articles__link">
+                        مهارت های نرم
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div class="course-info">
+                  <span class="course-info__courses-title">دوره های جدید</span>
+                  <ul class="sidebar-articles__list">
+                    <li class="sidebar-articles__item">
+                      <i class="fas fa-angle-left sidebar-articles__icon"></i>
+                      <a href="#" class="sidebar-articles__link">
+                        صفحه اصلی
+                      </a>
+                    </li>
+                    <li class="sidebar-articles__item">
+                      <i class="fas fa-angle-left sidebar-articles__icon"></i>
+                      <a href="#" class="sidebar-articles__link">
+                        فرانت اند
+                      </a>
+                    </li>
+                    <li class="sidebar-articles__item">
+                      <i class="fas fa-angle-left sidebar-articles__icon"></i>
+                      <a href="#" class="sidebar-articles__link">
+                        امنیت
+                      </a>
+                    </li>
+                    <li class="sidebar-articles__item">
+                      <i class="fas fa-angle-left sidebar-articles__icon"></i>
+                      <a href="#" class="sidebar-articles__link">
+                        پایتون
+                      </a>
+                    </li>
+                    <li class="sidebar-articles__item">
+                      <i class="fas fa-angle-left sidebar-articles__icon"></i>
+                      <a href="#" class="sidebar-articles__link">
+                        مهارت های نرم
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
