d89040ba826efed9b02feeeb05d0b08808a0fd26
Mohammad Hosein <en.mohammadhosein@gmail.com>
2023-07-31 23:02:14 +0330

    feat: image loader

--- src/Components/ArticleBox/ArticleBox.jsx
+++ src/Components/ArticleBox/ArticleBox.jsx
@@ -1,7 +1,14 @@
-import React from "react";
-import './ArticleBox.css'
+import React, { useState } from "react";
+import "./ArticleBox.css";
+import CircleSpinner from "../CircleSpinner/CircleSpinner";
 
-export default function ArticleBox({title , desc , cover}) {
+export default function ArticleBox({ title, desc, cover }) {
+  const [isImgShow, setIsImgShow] = useState(false);
+
+  const onImageLoader = () => setIsImgShow(true);
+  const onImageError = () => {
+    alert("Error In Loading Image");
+  };
   return (
     <div class="col-4">
       <div class="article-card">
@@ -11,16 +18,17 @@ export default function ArticleBox({title , desc , cover}) {
               src={cover}
               class="article-card__img"
               alt="Article Cover"
+              onLoad={() => onImageLoader()}
+              onError={() => onImageError()}
             />
+            {!isImgShow && <CircleSpinner />}
           </a>
         </div>
         <div class="article-card__content">
           <a href="#" class="article-card__link">
             {title}
           </a>
-          <p class="article-card__text">
-           {desc}
-          </p>
+          <p class="article-card__text">{desc}</p>
           <a href="#" class="article-card__btn">
             بیشتر بخوانید
           </a>
--- src/Components/CourseBox/CourseBox.jsx
+++ src/Components/CourseBox/CourseBox.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CourseBox.css";
+import CircleSpinner from "../CircleSpinner/CircleSpinner";
 
 export default function CourseBox() {
+  const [isImgShow , setIsImgShow] = useState(false)
+
+  const onImageLoader = () => setIsImgShow(true)
+  const onImageError = () => {
+    alert('Error In Loading Image')
+  }
+
   return (
     <div class="col-4">
       <div class="course-box">
@@ -10,7 +18,14 @@ export default function CourseBox() {
             src="/images/courses/fareelancer.png"
             alt="Course img"
             class="course-box__img"
+            onLoad={() => onImageLoader()}
+            onError={() => onImageError()}
           />
+          {
+            !isImgShow && (
+              <CircleSpinner/>
+            )
+          }
         </a>
         <div class="course-box__main">
           <a href="#" class="course-box__title">
