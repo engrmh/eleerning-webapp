0dbb157815ce5a1eb0d886c9f5f74fd15d324ff6
GALAHAD <59095451+engrmh@users.noreply.github.com>
2023-08-22 23:49:42 +0330

    feat: sorting category

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,54 +1,61 @@
-import React, {useState , useEffect} from "react";
-import "./Pagination.css";
-import {Link, useParams} from "react-router-dom";
-import app from "../../App";
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 
-function Pagination({items , itemsCount , pathName , setShownItems}) {
-  const {page} = useParams()
-  const [pageCount, setPageCount] = useState(null)
+import "./Pagination.css";
+
+export default function Pagination({
+                                     items,
+                                     itemsCount,
+                                     pathname,
+                                     setShownCourses,
+                                   }) {
+  const [pagesCount, setPagesCount] = useState(null); // 3
+  const { page } = useParams();
 
   useEffect(() => {
-    let endIndex = itemsCount * page //=> 3 * 1 -> items -> [1 , 3]
-    let startIndex = endIndex - itemsCount //=> 3 - 3 = 0 -> [0,3]
-    let paginatedItems = items.slice(startIndex , endIndex)
-    setShownItems(paginatedItems)
-    let pagesNumber = Math.ceil(items.length / itemsCount)
-    setPageCount(pagesNumber)
-  }, [page , items]);
+    let endIndex = itemsCount * page;
+    let startIndex = endIndex - itemsCount;
+    let paginatedItems = items.slice(startIndex, endIndex);
+    setShownCourses(paginatedItems);
+
+    let pagesNumber = Math.ceil(items.length / itemsCount);
+    setPagesCount(pagesNumber);
+  }, [page, items]);
 
   return (
-    <div className="courses-pagination">
-      <ul className="courses__pagination-list">
-        {/*<li className="courses__pagination-item">*/}
-        {/*  <a href="#" className="courses__pagination-link">*/}
-        {/*    <i className="fas fa-long-arrow-alt-right courses__pagination-icon"></i>*/}
-        {/*  </a>*/}
-        {/*</li>*/}
-        {
-          Array(pageCount).fill(0).map((item , index) => (
-              <li className="courses__pagination-item" key={index}>
-                {
-                  index + 1 === Number(page) ? (
-                      <Link to={`${pathName}/${index + 1}`} className="courses__pagination-link courses__pagination-link--active">
-                        {index + 1}
-                      </Link>
-                  ) : (
-                      <Link to={`${pathName}/${index + 1}`} className="courses__pagination-link">
-                        {index + 1}
-                      </Link>
-                  )
-                }
-              </li>
-          ))
-        }
-        {/*<li className="courses__pagination-item">*/}
-        {/*  <a href="#" className="courses__pagination-link">*/}
-        {/*    <i className="fas fa-long-arrow-alt-left courses__pagination-icon"></i>*/}
-        {/*  </a>*/}
-        {/*</li>*/}
-      </ul>
-    </div>
+      <div className="courses-pagination">
+        <ul className="courses__pagination-list">
+          {Array(pagesCount)
+              .fill(0)
+              .map((item, index) => (
+                  <li className="courses__pagination-item">
+                    {index + 1 === Number(page) ? (
+                        <Link
+                            to={`${pathname}/${index + 1}`}
+                            className="courses__pagination-link courses__pagination-link--active"
+                        >
+                          {index + 1}
+                        </Link>
+                    ) : (
+                        <Link
+                            to={`${pathname}/${index + 1}`}
+                            className="courses__pagination-link"
+                        >
+                          {index + 1}
+                        </Link>
+                    )}
+                  </li>
+              ))}
+
+          {/* <li className="courses__pagination-item">
+          <a
+            href="#"
+            className="courses__pagination-link courses__pagination-link--active"
+          >
+            3
+          </a>
+        </li> */}
+        </ul>
+      </div>
   );
 }
-
-export default Pagination;
--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -1,4 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import Topbar from "./../../Components/Topbar/Topbar";
+import Navbar from "./../../Components/Navbar/Navbar";
+import Footer from "./../../Components/Footer/Footer";
+
 import "./Category.css";
 import CourseBox from "../../Components/CourseBox/CourseBox";
 import Pagination from "../../Components/Pagination/Pagination";
@@ -6,91 +10,159 @@ import { useParams } from "react-router-dom";
 
 export default function Category() {
   const [courses, setCourses] = useState([]);
+  const [orderedCourses, setOrderedCourses] = useState([]);
   const [shownCourses, setShownCourses] = useState([])
+  const [status, setStatus] = useState('default')
+  const [statusTitle, setStatusTitle] = useState('مرتب سازی پیش فرض')
+
   const { categoryName } = useParams();
+
   useEffect(() => {
     fetch(`http://localhost:4000/v1/courses/category/${categoryName}`)
-      .then((res) => res.json())
-      .then((data) => setCourses(data));
+        .then((res) => res.json())
+        .then((allCourses) => {
+          console.log(allCourses);
+          setCourses(allCourses);
+          setOrderedCourses(allCourses)
+        });
   }, [categoryName]);
 
-  return (
-    <>
-      <section className="courses">
-        <div className="container">
-          <div className="courses-content">
-            <div className="container">
-              <div className="row">
-                {courses.length === 0 ? (
-                  <div className="alert alert-warning mt-4">
-                    هیچ دوره ای برای این دسته بندی وجود ندارد
-                  </div>
-                ) : (
-                  <>
-                    <div className="courses-top-bar px-4">
-                      <div className="courses-top-bar__right">
-                        <div className="courses-top-bar__row-btn courses-top-bar__icon--active">
-                          <i className="fas fa-border-all courses-top-bar__icon"></i>
-                        </div>
-                        <div className="courses-top-bar__column-btn">
-                          <i className="fas fa-align-left courses-top-bar__icon"></i>
-                        </div>
+  useEffect(() => {
+    switch(status) {
+      case 'free': {
+        const freeCourses = courses.filter(course => course.price === 0)
+        setOrderedCourses(freeCourses)
+        break
+      }
+      case 'money': {
+        const notFreeCourses = courses.filter(course => course.price !== 0)
+        setOrderedCourses(notFreeCourses)
+        break
+      }
+      case 'last': {
+        setOrderedCourses(courses)
+        break
+      }
+      case 'first': {
+        const reversedCourses = courses.slice().reverse()
+        setOrderedCourses(reversedCourses)
+        break
+      }
+      default: {
+        setOrderedCourses(courses)
+      }
+    }
+  }, [status])
 
-                        <div className="courses-top-bar__selection">
+  const statusTitleChangeHandler = event => {
+    setStatusTitle(event.target.textContent)
+  }
+
+  return (
+      <>
+        <section className="courses">
+          <div className="container">
+            <div className="courses-content">
+              <div className="container">
+                <div className="row">
+                  {courses.length === 0 ? (
+                      <div className="alert alert-warning">
+                        هنوز هیچ دوره‌ای برای این کتگوری وجود ندارد
+                      </div>
+                  ) : (
+                      <>
+                        <div className="courses-top-bar">
+                          <div className="courses-top-bar__right">
+                            <div className="courses-top-bar__row-btn courses-top-bar__icon--active">
+                              <i className="fas fa-border-all courses-top-bar__icon"></i>
+                            </div>
+                            <div className="courses-top-bar__column-btn">
+                              <i className="fas fa-align-left courses-top-bar__icon"></i>
+                            </div>
+
+                            <div className="courses-top-bar__selection">
                           <span className="courses-top-bar__selection-title">
-                            مرتب سازی پیش فرض
+                            {/* مرتب سازی پیش فرض */}
+                            {
+                              statusTitle
+                            }
                             <i className="fas fa-angle-down courses-top-bar__selection-icon"></i>
                           </span>
-                          <ul className="courses-top-bar__selection-list">
-                            <li className="courses-top-bar__selection-item courses-top-bar__selection-item--active">
-                              مرتب سازی پیش فرض
-                            </li>
-                            <li className="courses-top-bar__selection-item">
-                              مربت سازی بر اساس محبوبیت
-                            </li>
-                            <li className="courses-top-bar__selection-item">
-                              مربت سازی بر اساس امتیاز
-                            </li>
-                            <li className="courses-top-bar__selection-item">
-                              مربت سازی بر اساس آخرین
-                            </li>
-                            <li className="courses-top-bar__selection-item">
-                              مربت سازی بر اساس ارزان ترین
-                            </li>
-                            <li className="courses-top-bar__selection-item">
-                              مربت سازی بر اساس گران ترین
-                            </li>
-                          </ul>
-                        </div>
-                      </div>
+                              <ul className="courses-top-bar__selection-list">
+                                <li className="courses-top-bar__selection-item courses-top-bar__selection-item--active" onClick={(event) => {
+                                  setStatus('مرتب سازی پیش فرض')
+                                  statusTitleChangeHandler(event)
+                                }}>
+                                  مرتب سازی پیش فرض
+                                </li>
+                                <li className="courses-top-bar__selection-item" onClick={(event) => {
+                                  setStatus('free')
+                                  statusTitleChangeHandler(event)
+                                }}>
+                                  مرتب سازی دوره های رایگان
+                                </li>
+                                <li className="courses-top-bar__selection-item" onClick={(event) => {
+                                  setStatus('money')
+                                  statusTitleChangeHandler(event)
+                                }}>
+                                  مرتب سازی دوره های پولی
+                                </li>
+                                <li className="courses-top-bar__selection-item" onClick={(event) => {
+                                  setStatus('last')
+                                  statusTitleChangeHandler(event)
+                                }}>
+                                  مرتب سازی بر اساس آخرین
+                                </li>
+                                <li className="courses-top-bar__selection-item" onClick={(event) => {
+                                  setStatus('first')
+                                  statusTitleChangeHandler(event)
+                                }}>
+                                  مرتب سازی بر اساس اولین
+                                </li>
+                                <li className="courses-top-bar__selection-item" onClick={(event) => {
+                                  setStatus('cheap')
+                                  statusTitleChangeHandler(event)
+                                }}>
+                                  مرتب سازی بر اساس ارزان ترین
+                                </li>
+                                <li className="courses-top-bar__selection-item" onClick={(event) => {
+                                  setStatus('expensive')
+                                  statusTitleChangeHandler(event)
+                                }}>
+                                  مرتب سازی بر اساس گران ترین
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
 
-                      <div className="courses-top-bar__left">
-                        <form action="#" className="courses-top-bar__form">
-                          <input
-                            type="text"
-                            className="courses-top-bar__input"
-                            placeholder="جستجوی دوره ..."
-                          />
-                          <i className="fas fa-search courses-top-bar__search-icon"></i>
-                        </form>
-                      </div>
-                    </div>
-                    {shownCourses.map((course) => (
-                      <CourseBox {...course} />
-                    ))}
-                    <Pagination
-                        items={courses}
-                        itemsCount={3}
-                        pathName={`/category-info/${categoryName}`}
-                        setShownCourses={setShownCourses}
-                    />
-                  </>
-                )}
+                          <div className="courses-top-bar__left">
+                            <form action="#" className="courses-top-bar__form">
+                              <input
+                                  type="text"
+                                  className="courses-top-bar__input"
+                                  placeholder="جستجوی دوره ..."
+                              />
+                              <i className="fas fa-search courses-top-bar__search-icon"></i>
+                            </form>
+                          </div>
+                        </div>
+                        {shownCourses.map((course) => (
+                            <CourseBox {...course} />
+                        ))}
+
+                        <Pagination
+                            items={orderedCourses}
+                            itemsCount={3}
+                            pathname={`/category-info/${categoryName}`}
+                            setShownCourses={setShownCourses}
+                        />
+                      </>
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-    </>
+        </section>
+      </>
   );
 }
