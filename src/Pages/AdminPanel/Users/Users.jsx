aa89e8034e052b983f0eadf30c65f49d2923722f
GALAHAD <59095451+engrmh@users.noreply.github.com>
2023-10-27 15:56:17 +0330

    feat: get users from server and showing in table

--- src/Pages/AdminPanel/Users/Users.jsx
+++ src/Pages/AdminPanel/Users/Users.jsx
@@ -1,42 +1,62 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import DataTable from "../../../Components/AdminPanel/DataTable/DataTable";
+import AuthContext from "../../../Components/context/authContext";
 
 export default function Users() {
+  const [allUsers, setAllUsers] = useState([]);
+  useEffect(() => {
+    const localStorageData = JSON.parse(localStorage.getItem("user"));
+    fetch(`http://localhost:4000/v1/users`, {
+      headers: {
+        authorization: `Bearer ${localStorageData.token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setAllUsers(data);
+      });
+  }, []);
   return (
     <>
       <DataTable title="کاربران">
-        <table class="table">
+        <table className="table">
           <thead>
             <tr>
-              <th>شناسه</th>
-              <th>نام</th>
-              <th>نام خانوادگی</th>
-              <th>شماره</th>
-              <th>ایمیل</th>
-              <th>رمز عبور</th>
-              <th>ویرایش</th>
-              <th>حذف</th>
+              <th className="text-black bg-info">شناسه</th>
+              <th className="text-black bg-info">نام و نام خانوادگی</th>
+              {/*<th className="text-black bg-info">شماره</th>*/}
+              <th className="text-black bg-info">ایمیل</th>
+              <th className="text-black bg-info">نام کاربری</th>
+              <th className="text-black bg-info">ویرایش</th>
+              <th className="text-black bg-info">حذف</th>
+              <th className="text-black bg-info">مسدود</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>34223</td>
-              <td>علی</td>
-              <td>سعیدی</td>
-              <td>09123443243</td>
-              <td>ali@gmail.com</td>
-              <td>ehsan1323</td>
-              <td>
-                <button type="button" class="btn btn-primary edit-btn">
-                  ویرایش
-                </button>
-              </td>
-              <td>
-                <button type="button" class="btn btn-danger delete-btn">
-                  حذف
-                </button>
-              </td>
-            </tr>
+            {allUsers.map((user, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                {/*<td>{user.phone}</td>*/}
+                <td>{user.email}</td>
+                <td>{user.username}</td>
+                <td>
+                  <button type="button" className="btn btn-primary edit-btn">
+                    ویرایش
+                  </button>
+                </td>
+                <td>
+                  <button type="button" className="btn btn-danger delete-btn">
+                    حذف
+                  </button>
+                </td>
+                <td>
+                  <button type="button" className="btn btn-danger delete-btn">
+                    مسدود
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </DataTable>
