033ecca5aa801fe32072d1d4112f65bef266cf34
GALAHAD <59095451+engrmh@users.noreply.github.com>
2023-10-28 11:05:07 +0330

    feat: add new user

--- src/Pages/AdminPanel/Users/Users.jsx
+++ src/Pages/AdminPanel/Users/Users.jsx
@@ -1,8 +1,41 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import DataTable from "../../../Components/AdminPanel/DataTable/DataTable";
+import { useForm } from "../../../hooks/useForm";
+import Input from "../../../Components/Form/Input";
+import {
+  emailValidator,
+  maxValidator,
+  minValidator,
+  requiredValidator,
+} from "../../../Validators/rules";
 
 export default function Users() {
   const [allUsers, setAllUsers] = useState([]);
+  const [formState, onInputHandler] = useForm(
+    {
+      name: {
+        value: "",
+        isValid: false,
+      },
+      username: {
+        value: "",
+        isValid: false,
+      },
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+      phone: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
   const localStorageData = JSON.parse(localStorage.getItem("user"));
   useEffect(() => {
     getAllUsers();
@@ -17,6 +50,29 @@ export default function Users() {
       .then((res) => res.json())
       .then((data) => {
         setAllUsers(data);
+      });
+  };
+  const registerNewUser = (event) => {
+    event.preventDefault();
+    const newUserInfo = {
+      name: `${formState.inputs.name.value}`,
+      username: formState.inputs.username.value,
+      email: formState.inputs.email.value,
+      phone: formState.inputs.phone.value,
+      password: formState.inputs.password.value,
+      confirmPassword: formState.inputs.password.value,
+    };
+
+    fetch("http://localhost:4000/v1/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUserInfo),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        getAllUsers();
       });
   };
   const userRemoveHandler = (userID) => {
@@ -72,8 +128,120 @@ export default function Users() {
       }
     });
   };
+
   return (
     <>
+      <div className="home-content-edit">
+        <div className="back-btn">
+          <i className="fas fa-arrow-right"></i>
+        </div>
+        <form className="form">
+          <div className="col-6">
+            <div className="name input">
+              <label className="input-title">نام و نام خانوادگی</label>
+              <Input
+                type="text"
+                className=""
+                id="name"
+                element="input"
+                validations={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(20),
+                ]}
+                onInputHandler={onInputHandler}
+                placeholder="لطفا نام و نام خانوادگی کاربر را وارد کنید..."
+              />
+              <span className="error-message text-danger"></span>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="family input">
+              <label className="input-title">نام کاربری</label>
+              <Input
+                type="text"
+                className=""
+                id="username"
+                element="input"
+                validations={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(20),
+                ]}
+                onInputHandler={onInputHandler}
+                placeholder="لطفا نام کاربری را وارد کنید..."
+              />
+              <span className="error-message text-danger"></span>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="email input">
+              <label className="input-title">ایمیل</label>
+              <Input
+                type="text"
+                className=""
+                id="email"
+                element="input"
+                validations={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(20),
+                  emailValidator(),
+                ]}
+                onInputHandler={onInputHandler}
+                placeholder="لطفا ایمیل کاربر را وارد کنید..."
+              />
+              <span className="error-message text-danger"></span>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="password input">
+              <label className="input-title">رمز عبور</label>
+              <Input
+                type="text"
+                className=""
+                id="password"
+                element="input"
+                validations={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(20),
+                ]}
+                onInputHandler={onInputHandler}
+                placeholder="لطفا رمز عبور کاربر را وارد کنید..."
+              />
+              <span className="error-message text-danger"></span>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="phone input">
+              <label className="input-title">شماره تلفن</label>
+              <Input
+                type="text"
+                className=""
+                id="phone"
+                element="input"
+                validations={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(20),
+                ]}
+                onInputHandler={onInputHandler}
+                placeholder="لطفا شماره تلفن کاربر را وارد کنید..."
+              />
+              <span className="error-message text-danger"></span>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="bottom-form">
+              <div className="submit-btn">
+                <input type="submit" value="افزودن" onClick={registerNewUser} />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+
       <DataTable title="کاربران">
         <table className="table">
           <thead>
