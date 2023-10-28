3f5c9bcd92ad12982285d36102af402cb61289f0
GALAHAD <59095451+engrmh@users.noreply.github.com>
2023-10-28 11:40:18 +0330

    feat: add new category

--- src/Pages/AdminPanel/Category/Category.jsx
+++ src/Pages/AdminPanel/Category/Category.jsx
@@ -1,8 +1,30 @@
 import React, { useEffect, useState } from "react";
 import DataTable from "../../../Components/AdminPanel/DataTable/DataTable";
+import Input from "../../../Components/Form/Input";
+import {
+  requiredValidator,
+  minValidator,
+  maxValidator,
+} from "../../../Validators/rules";
+import { useForm } from "../../../hooks/useForm";
+import swal from "sweetalert";
+import "./Category.css";
 
 export default function CategoryAPanel() {
   const [categories, setCategories] = useState([]);
+  const [formState, onInputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      shortname: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
 
   useEffect(() => {
     getAllCategories();
@@ -16,8 +38,87 @@ export default function CategoryAPanel() {
       });
   }
 
+  const createNewCategory = (event) => {
+    event.preventDefault();
+    const localStorageData = JSON.parse(localStorage.getItem("user"));
+
+    const newCategoryInfo = {
+      title: formState.inputs.title.value,
+      name: formState.inputs.shortname.value,
+    };
+
+    fetch("http://localhost:4000/v1/category", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorageData.token}`,
+      },
+      body: JSON.stringify(newCategoryInfo),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        swal({
+          title: "دسته بندی مورد نظر با موفقیت اضافه شد",
+          icon: "success",
+          buttons: "اوکی",
+        }).then(() => {
+          getAllCategories();
+        });
+      });
+  };
+
   return (
     <>
+      <div className="container-fluid" id="home-content">
+        <div className="container">
+          <div className="home-title">
+            <span>افزودن دسته‌بندی جدید</span>
+          </div>
+          <form className="form">
+            <div className="col-6">
+              <div className="name input">
+                <label className="input-title">عنوان</label>
+                <Input
+                  element="input"
+                  id="title"
+                  type="text"
+                  placeholder="لطفا عنوان را وارد کنید..."
+                  validation={[minValidator(5), maxValidator(20)]}
+                  onInputHandler={onInputHandler}
+                />
+                <span className="error-message text-danger"></span>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="name input">
+                <label className="input-title">اسم کوتاه</label>
+                <Input
+                  element="input"
+                  id="shortname"
+                  type="text"
+                  placeholder="لطفا عنوان را وارد کنید..."
+                  validation={[minValidator(5), maxValidator(30)]}
+                  onInputHandler={onInputHandler}
+                />
+                <span className="error-message text-danger"></span>
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="bottom-form">
+                <div className="submit-btn">
+                  <input
+                    type="submit"
+                    value="افزودن"
+                    onClick={createNewCategory}
+                  />
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+
       <DataTable title="دسته‌بندی‌ها">
         <table className="table">
           <thead>
@@ -30,7 +131,7 @@ export default function CategoryAPanel() {
           </thead>
           <tbody>
             {categories.map((category, index) => (
-              <tr>
+              <tr key={index}>
                 <td>{index + 1}</td>
                 <td>{category.title}</td>
                 <td>
